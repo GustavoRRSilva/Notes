18409d07e75542fe444c03f98d3573fd6166be51
Gustavo <gstvrrds@gmail.com>
2024-02-26 16:43:02 -0300

    feature:adicionando campos de login e senha

---
+++ src/Componentes/InputText/InputText.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './InputText.css'
+const InputText = ({type,placeholder})=>{
+    return(
+        <>
+        <input type = {type} name="" id="" className="InputTextLogin" placeholder = {placeholder}/>
+        </>
+    )
+}
+export default InputText;
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,19 +1,26 @@
 import React from "react";
-import './Login.css'
+import "./Login.css";
 import ButtonInfo from "../../Componentes/ButtonInfo/ButtonInfo";
-const Login = ({name,img,src,alt})=>{
-    return(
-        <div className="Background">
-            <div className="infosLogin">
-            <h2>Welcome back!</h2>
-            <ButtonInfo name = 'Entre com o google' src="src\assets\iconGoogle.svg" alt = 'Icone do google'/>
-            <div className="or">
-                <span className="line"></span>
-                <h3>or</h3>
-                <span className="line"></span>
-            </div>
-            </div>
+import InputText from "../../Componentes/InputText/InputText";
+const Login = ({ name, img, src, alt,inputType}) => {
+  return (
+    <div className="Background">
+      <div className="infosLogin">
+        <h2>Welcome back!</h2>
+        <ButtonInfo
+          name="Entre com o google"
+          src="src\assets\iconGoogle.svg"
+          alt="Icone do google"
+        />
+        <div className="or">
+          <span className="line"></span>
+          <h3>or</h3>
+          <span className="line"></span>
         </div>
-    )
-}
+        <InputText type = "Text" placeholder = "Seu email"/>
+        <InputText type = "password" placeholder = "Sua senha"/>
+      </div>
+    </div>
+  );
+};
 export default Login;
