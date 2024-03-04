175ce2d61e8e314a72c6ef97986467fd13e240c1
Gustavo <gstvrrds@gmail.com>
2024-03-04 09:52:42 -0300

    feat:adicionando responsividade no login

--- src/Componentes/ButtonInfo/ButtonInfo.jsx
+++ src/Componentes/ButtonInfo/ButtonInfo.jsx
@@ -3,14 +3,14 @@ import styles from "./ButtonInfo.module.css";
 
 const ButtonInfo = ({ name, img, src, alt }) => {
   return (
-    <div className={styles.ButtonInfoPai}>
+  
       <button className={styles.ButtonInfoFilho}>
         {{ img } ? (
           <img src={src} alt={alt} className={styles.iconButton} />
         ) : ""}
-        {name}
+       <p> {name}</p>
       </button>
-    </div>
+  
   );
 };
 export default ButtonInfo;
